8c2cf51f71f17fb00a77eeb7b58d58e9f3074e35
Jonas Scherer <jonas@matilhaestud.io>
2017-06-10 17:41:09 -0300

    adding stocks controller

---
+++ app/models/stock.rb
@@ -0,0 +1,25 @@
+class Stock < ApplicationRecord
+	
+	def self.find_by_ticker(ticker_symbol)
+		where(ticker: ticker_symbol).first
+	end
+  
+  def self.new_from_lookup(ticker_symbol)
+  	stock = StockQuote::Stock.quote(ticker_symbol)
+  	return nil unless stock.name 
+  	new_stock = new(ticker: stock.symbol, name: stock.name)
+  	new_stock.last_price = new_stock.price
+  	new_stock
+  end
+
+  def price
+  	closing = StockQuote::Stock.quote(ticker).close
+  	return "#{closing} (closing)" if closing
+
+  	opening = StockQuote::Stock.quote(ticker).open
+  	return "#{opening} (opening)" if opening
+
+    "Unavailable"
+   end
+
+end
