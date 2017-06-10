a60d82d03b88a191f11f576be28919a77c5549a9
Jonas Scherer <jonas@matilhaestud.io>
2017-06-10 19:01:54 -0300

    ajax for search

---
+++ app/assets/javascripts/stocks.js
@@ -0,0 +1,33 @@
+var init_stock_lookup;
+
+init_stock_lookup = function(){
+
+	$('#stock-lookup-form').on('ajax:before', function(event, data, status){
+		show_spinner();
+	});
+
+	$('#stock-lookup-form').on('ajax:after', function(event, data, status){
+		hide_spinner();
+	});
+	
+	$('#stock-lookup-form').on('ajax:success', function(event, data, status){
+		if(data){
+			$('#stock-lookup').replaceWith(data);
+			init_stock_lookup();
+		}else{
+			$('#stock-lookup-results').replaceWith('');
+			$('#stock-lookup-errors').replaceWith("Stock not found");
+		}
+
+	});
+
+	$('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+		$('#stock-lookup-results').replaceWith('');
+		$('#stock-lookup-errors').replaceWith("Stock not found");
+	});
+
+}
+
+$(document).ready(function(){
+	init_stock_lookup();
+});
