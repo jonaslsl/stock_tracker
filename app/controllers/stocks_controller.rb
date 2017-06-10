dca095801e149a6b1921ea8b97fe36298f3c5494
Jonas Scherer <jonas@matilhaestud.io>
2017-06-10 18:02:47 -0300

    showing search results partial

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,8 +4,8 @@ class StocksController < ApplicationController
   		@stock = Stock.find_by_ticker(params[:stock])
   		@stock ||= Stock.new_from_lookup(params[:stock])
   		if @stock 
-  			render json: @stock
-  			#render partial :lookup 
+  			# render json: @stock
+  			render partial: 'lookup'
   		end
   	else
   		render status: :not_found, nothing: true
