67de77b70c4674f25464eae36f5455c2734c39be
Jonas Scherer <jonas@matilhaestud.io>
2017-06-11 01:17:57 -0300

    search friend, add friend and show friend

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   resources :user_stocks, except: [:show, :edit, :update]
   resources :users, only: [:show]
   resources :friendship
-  
+
   root 'welcome#index'
   
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'my_friends', to: 'users#my_friends'
   get 'search_stocks', to: 'stocks#search'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
 
 end
