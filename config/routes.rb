936aa7fcb2c60662ab2fb8e88a89d695dd666e4e
Jonas Scherer <jonas@matilhaestud.io>
2017-06-11 00:25:25 -0300

    friends view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: { :registrations => 'user/registrations' }
   resources :user_stocks, except: [:show, :edit, :update]
-
+  resources :users, only: [:show]
+  resources :friendship
+  
   root 'welcome#index'
   
   get 'my_portfolio', to: 'users#my_portfolio'
